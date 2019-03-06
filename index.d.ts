141a02b3482c6c9185a0f999a580bf3c9ae7254e
Robert Jackson <rjackson@linkedin.com>
2019-03-06 10:02:53 -0500

    Fix TS conversion error causing getRealDirectoryPath to be undefined.

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,6 @@ export = resolvePackagePath;
 declare function resolvePackagePath(target: string, basedir: string, _cache?: CacheGroup | boolean): any;
 declare namespace resolvePackagePath {
     var _resetCache: () => void;
+    var getRealFilePath: (filePath: string) => any;
+    var getRealDirectoryPath: (directoryhPath: string) => any;
 }
